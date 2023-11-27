f8d70d50d14fdc25275e8cf025efe2246ab8260c
djkeu <mrkooij@gmail.com>
2023-11-27 20:23:09 +0100

    Alternative time assignement to righ_this_minute

--- ch01/odd.py
+++ ch01/odd.py
@@ -6,6 +6,12 @@ odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
 
 right_this_time = datetime.today().minute
 
+"""
+Alt:
+time_now = datetime.today()
+right_this_minute = time_now.minute
+"""
+
 if right_this_time in odds:
     print("Deze minuut ziet er nogal oneven uit.")
 else:
