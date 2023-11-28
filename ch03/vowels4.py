f7bfd59f936501c6b7598fd8866d82761e3b0d82
djkeu <mrkooij@gmail.com>
2023-11-28 23:16:38 +0100

    Use the setdefault() method

--- ch03/vowels4.py
+++ ch03/vowels4.py
@@ -3,14 +3,9 @@ word = input('Voer een woord of zin in om klinkers in te kunnen vinden: ')
 
 found = {}
 
-found['a'] = 0
-found['e'] = 0
-found['i'] = 0
-found['o'] = 0
-found['u'] = 0
-
 for letter in word:
     if letter in vowels:
+        found.setdefault(letter, 0)
         found[letter] +=1
 
 for k, v in sorted(found.items()):
