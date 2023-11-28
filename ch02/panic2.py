0f9377c59963b5fa6acdf4e654afc428467c23a4
djkeu <mrkooij@gmail.com>
2023-11-28 19:33:47 +0100

    Start Sharpen your pencil p.81

--- ch02/panic2.py
+++ ch02/panic2.py
@@ -22,3 +22,33 @@ plist.insert(-1, plist.pop())
 new_phrase = ''.join(plist)
 print(plist)
 print(new_phrase)
+
+
+# Sharpen your pencil, p.81
+print("\nSharpen your pencil, p.81")
+
+phrase = "Don't panic!"
+plist = list(phrase)
+
+print(phrase)
+print(plist)
+
+# Begin code to change
+"""
+for i in range(4):
+    plist.pop()
+    print(plist)
+plist.pop(0)
+plist.remove("'")
+
+
+plist.extend([plist.pop(), plist.pop()])
+plist.insert(2, plist.pop(3))
+
+new_phrase = ''.join(plist)
+"""
+# End code to change
+
+
+print(plist)
+print(new_phrase)
