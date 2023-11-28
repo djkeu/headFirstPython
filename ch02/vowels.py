874709da5de1e1bab9fb6384101768dcb3b8b27b
djkeu <mrkooij@gmail.com>
2023-11-28 14:12:33 +0100

    Move code from vowels to vowels3

--- ch02/vowels.py
+++ ch02/vowels.py
@@ -1,20 +1,6 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 word = "Marc Kooij"
 
-#1
 for letter in word:
     if letter in vowels:
         print(letter)
-
-#2
-print()
-found_vowels = []
-word = input("Noem eens woord of een zin, dat zeg ik welke klinkers er in zitten: ")
-
-for letter in word:
-    if letter in vowels:
-        if letter not in found_vowels:
-            found_vowels.append(letter)
-
-for vowel in found_vowels:
-    print(vowel)
