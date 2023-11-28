46652a4904ce9ecea03aad1e13366e8616cdd780
djkeu <mrkooij@gmail.com>
2023-11-28 13:55:12 +0100

    Find unique vowels

--- ch02/bits_and_ends.py
+++ ch02/bits_and_ends.py
@@ -1,6 +1,16 @@
-vowels = ['a', 'e', 'i', 'o', 'u']
-word = "Marc Kooij"
+found = []
+print(len(found))
 
-for letter in word:
-    if letter in vowels:
-        print(letter)
+found.append('a')
+print(len(found))
+print(found)
+
+found.append('e')
+found.append('i')
+found.append('o')
+print(len(found))
+print(found)
+
+if 'u' not in found:
+    found.append('u')
+print(found)
--- ch02/vowels.py
+++ ch02/vowels.py
@@ -1,6 +1,19 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 word = "Marc Kooij"
 
+#1
 for letter in word:
     if letter in vowels:
         print(letter)
+
+#2
+print()
+found_vowels = []
+
+for letter in word:
+    if letter in vowels:
+        if letter not in found_vowels:
+            found_vowels.append(letter)
+
+for vowel in found_vowels:
+    print(vowel)
