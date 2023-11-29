c6a6a9a31a68a332cbe2cc39b491dfa76666222e
djkeu <mrkooij@gmail.com>
2023-11-29 22:55:43 +0100

    Use pycodestyle to find pep8 recoomendations

--- ch04/vsearch_orig.py
+++ ch04/vsearch_orig.py
@@ -1,12 +1,13 @@
-def search_vowels(phrase:str) -> set:
+def search_vowels(phrase: str) -> set:
     """Search for vowels in a word or sentence."""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
 
 
-def search_letters(phrase:str, letters:str='aeiou') -> set:
+def search_letters(phrase: str, letters: str = 'aeiou') -> set:
     """Return set of letters found in a phrase."""
     return set(letters).intersection(set(phrase))
+
 
 result_1 = search_letters('marc', 'abc')
 result_2 = search_letters('the eternal sunshine of the spotless mind', 'aeiou')
