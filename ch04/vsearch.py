fa83c15eb1b434b500bee632c296e93983651c8b
djkeu <mrkooij@gmail.com>
2023-11-29 14:08:30 +0100

    Create search_vowels()

---
+++ ch04/vsearch.py
@@ -0,0 +1,8 @@
+def search_vowels():
+    """Search for vowlels in a word or sentence."""
+    vowels = set('aeiou')
+    word = input("Voer een woord of een zin in: ")
+    found = vowels.intersection(set(word))
+    for vowel in found:
+        print(vowel)
+
