092510780cd20628bf6324bc0a70990d7925b4d0
djkeu <mrkooij@gmail.com>
2023-11-29 13:49:23 +0100

    Dict in dict, persons in people

--- ch03/bits3.py
+++ ch03/bits3.py
@@ -15,3 +15,37 @@ print(person3)
 
 # p.104
 # Shell stuff
+
+
+# p.136
+people = {}
+people['Ford'] = {
+    'Name': 'Ford Perfect',
+    'Gender': "Male",
+    'Occupation': "Researcher",
+    'Home Planet': "Betelgeuze Seven",
+}
+
+print()
+print(people)
+
+people["Arthur"] = {
+    'Name': 'Arthur Dent',
+    'Gender': 'Male',
+    'Occupation': 'Sandwich-Maker',
+    'Home Planet': 'Earth',
+}
+people["Trillian"] = {
+    'Name': 'Tricia McMillian',
+    'Gender': 'Female',
+    'Occupation': 'Mathematician',
+    'Home Planet': 'Earth',
+}
+people["Robot"] = {
+    'Name': 'Marvin',
+    'Gender': 'Unknown',
+    'Occupation': 'Paranoid Android',
+    'Home Planet': 'Unknown',
+}
+
+print(people)
