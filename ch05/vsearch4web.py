a6a299fee2be30410f72f99d92ccb52e2e324a75
djkeu <mrkooij@gmail.com>
2023-11-30 17:41:17 +0100

    Add methods= to do_search()

--- ch05/vsearch4web.py
+++ ch05/vsearch4web.py
@@ -1,5 +1,4 @@
-from flask import Flask
-
+from flask import Flask, render_template
 from vsearch import search_letters
 
 app = Flask(__name__)
@@ -8,8 +7,12 @@ app = Flask(__name__)
 def hello() -> str:
     return "Hello world from Flask."
 
-@app.route('/search4')
+@app.route('/search4', methods='POST')
 def do_search() -> str:
     return str(search_letters('marc kooij'))
+
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title="Welcom to search4letters on the web!")
 
 app.run()
