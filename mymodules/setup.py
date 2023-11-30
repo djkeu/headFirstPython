cec4461adbad863cd01676f5388fe9a8e7a4a450
djkeu <mrkooij@gmail.com>
2023-11-30 16:14:35 +0100

    Rename abundant vsearch files

--- mymodules/setup.py
+++ mymodules/setup.py
@@ -5,6 +5,7 @@ setup (
     version='1.0',
     description="The Head First Python Search tools",
     author='HF Python 2e',
+    author_email='hfpy2e@gmail.com',
     url='headfirstlabs.com',
-    py_modules='vsearch',
+    py_modules=['vsearch'],
 )
