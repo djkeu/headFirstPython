ee4e0281313f3d6c1c4417f4e5f668e3fa5408be
djkeu <mrkooij@gmail.com>
2023-12-01 13:03:17 +0100

    Copy ch06/vs*web.py code to webapp/vs*web.py

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -5,12 +5,18 @@ from vsearch import search_letters
 
 app = Flask(__name__)
 
+def log_request(req, res: str) -> None:
+    filename = 'vsearch.log'
+    with open(filename, 'a') as log:
+        print(req, res, file=log)
+
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title= "Here are you results:"
     results = str(search_letters(phrase, letters))
+    log_request(request, results)
 
     return render_template('results.html', 
                            the_phrase=phrase, 
@@ -22,6 +28,7 @@ def do_search() -> 'html':
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title="Welcom to search4letters on the web!")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
