83e5c4d3d6a5040f9a690b82e11b9fc7ba0e3834
djkeu <mrkooij@gmail.com>
2023-12-01 13:40:53 +0100

    pep8

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -5,29 +5,35 @@ from vsearch import search_letters
 
 app = Flask(__name__)
 
+
 def log_request(req, res: str) -> None:
     filename = 'vsearch.log'
     with open(filename, 'a') as log:
         print(req, res, file=log)
 
+
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    title= "Here are you results:"
+    title = "Here are you results:"
     results = str(search_letters(phrase, letters))
     log_request(request, results)
 
-    return render_template('results.html', 
-                           the_phrase=phrase, 
-                           the_letters=letters, 
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
                            the_title=title,
                            the_results=results,)
+
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title="Welcom to search4letters on the web!")
+    return render_template(
+        'entry.html',
+        the_title="Welcome to search4letters on the web!"
+        )
 
 
 if __name__ == '__main__':
