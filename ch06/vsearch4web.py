80624a0189f14ce3ccbc0e0073d469f9a60bb9d0
djkeu <mrkooij@gmail.com>
2023-12-01 12:59:54 +0100

    Call log_request() in do_search()

--- ch06/vsearch4web.py
+++ ch06/vsearch4web.py
@@ -5,12 +5,18 @@ from vsearch import search_letters
 
 app = Flask(__name__)
 
+def log_request(req: 'flask_request', res: str) -> None:
+    filename = 'vsearch.log'
+    with open(filename, 'a') as log:
+        print(req, res, file=log)
+
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title= "Here are you results:"
     results = str(search_letters(phrase, letters))
+    log_request(request, results)
 
     return render_template('results.html', 
                            the_phrase=phrase, 
@@ -23,12 +29,6 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html', the_title="Welcom to search4letters on the web!")
 
-def log_request(req: 'flask_request', res: str) -> None:
-    filename = 'vsearch.log'
-    with open(filename, 'a') as log:
-        print(req, res, file=log)
-
-log_request('request', 'result')
 
 if __name__ == '__main__':
     app.run(debug=True)
