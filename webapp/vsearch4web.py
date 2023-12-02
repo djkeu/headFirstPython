8e802a665f8e4c7bd61f0899fec58e4e5fd94b57
djkeu <mrkooij@gmail.com>
2023-12-02 12:32:03 +0100

    log_request(): str(dir(req))

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 def log_request(req, res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+        print(str(dir(req)), res, file=log)
 
 
 @app.route('/search4', methods=['POST'])
