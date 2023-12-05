995f4c7241217a2add4681c82b69c7f6052b8f62
djkeu <mrkooij@gmail.com>
2023-12-05 19:14:37 +0100

    Initialize UseDatabase

--- webapp/DBcm.py
+++ webapp/DBcm.py
@@ -2,12 +2,11 @@ import mysql.connector
 
 class UseDatabase:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, config: dict) -> None:
+        self.configuration = self.config
 
     def __enter__(self):
         pass
 
     def __exit__(self):
         pass
-    
