e575e275c4ca1d2d9f87bce1a082809bc4684de3
djkeu <mrkooij@gmail.com>
2023-12-05 19:03:51 +0100

    Set up Context manager

---
+++ webapp/DBcm.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+class UseDatabase:
+
+    def __init__(self) -> None:
+        pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self):
+        pass
+    
