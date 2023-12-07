f2b0c3d15e8c80c7d363585418e39e43424ec4ff
djkeu <mrkooij@gmail.com>
2023-12-07 23:13:13 +0100

    Create print_args2(), rename print_args1()

--- ch10/myfunc.py
+++ ch10/myfunc.py
@@ -1,7 +1,13 @@
 # p.390
 
-def myfunc(*args):
+def print_args1(*args):
     for a in args:
         print(a, end=' ')
     if args:
+        print()
+
+def print_args2(**kwargs):  # keyword args, for dicts
+    for k, v in kwargs:
+        print(k, v, sep='->', end=' ')
+    if kwargs:
         print()
