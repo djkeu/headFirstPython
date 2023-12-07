0a3b80de0a744bdea41d2b88cdb0e5bbaa0c82d6
djkeu <mrkooij@gmail.com>
2023-12-07 11:55:49 +0100

    Import flask session

--- ch10/simple_webapp.py
+++ ch10/simple_webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session
 
 app = Flask(__name__)
 
@@ -17,6 +17,8 @@ def page2() -> str:
 @app.route('/page3')
 def page3() -> str:
     return 'Derde set'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
