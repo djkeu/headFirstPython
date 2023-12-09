9c76ac317a048e6ec9288cb648212179a9dd0d6b
djkeu <mrkooij@gmail.com>
2023-12-09 15:17:06 +0100

    Create and test print_args3()

--- ch10/myfunc.py
+++ ch10/myfunc.py
@@ -11,3 +11,13 @@ def print_args2(**kwargs):  # keyword args, for dicts
         print(k, v, sep='->', end=' ')
     if kwargs:
         print()
+
+def print_args3(*args, **kwargs):
+    if args:
+        for a in args:
+            print(a, end=' ')
+        print()
+    if kwargs:
+        for k, v in kwargs.items():
+            print(k, v, sep='->', end=' ')
+        print()
