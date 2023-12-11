1a2159392df4d15d71ab46c9991818bc22a33454
djkeu <mrkooij@gmail.com>
2023-12-11 13:23:32 +0100

    Create class CredentialsError()

--- webapp/DBcm.py
+++ webapp/DBcm.py
@@ -4,6 +4,12 @@ import mysql.connector
 class ConnectionError(Exception):
     pass
 
+class CredentialsError(Exception):
+    pass
+
+class SQLError(Exception):
+    pass
+
 
 class UseDatabase:
 
@@ -14,10 +20,11 @@ class UseDatabase:
         try:
             self.conn = mysql.connector.connect(**self.configuration)
             self.cursor = self.conn.cursor()
-
             return self.cursor
         except mysql.connector.errors.InterfaceError as err:
             raise ConnectionError(err)
+        except mysql.connector.errors.ProgrammingError as err:
+            raise CredentialsError(err)
 
     def __exit__(self, exc_type, exc_value, exc_trace) -> None:
         self.conn.commit()
