006921af8c7f6b935cdc3cd412e7f42ae68f6ea8
djkeu <mrkooij@gmail.com>
2023-12-11 11:25:28 +0100

    Comment out raised Exeption in log_request()

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -28,7 +28,7 @@ def do_logout() -> str:
 
 def log_request(req, res: str) -> None:
     """Log details of the web request and the results."""
-    raise Exception("Something awful just happened.")
+    # raise Exception("Something awful just happened.")
 
     with UseDatabase(app.config['dbconfig']) as cursor:
         _sql = """insert into log
