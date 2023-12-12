a7ad2c2818a02451822ff36271d8a494fe005253
djkeu <mrkooij@gmail.com>
2023-12-12 17:36:25 +0100

    Add prints

--- ch12/sharpen_your_pencil.py
+++ ch12/sharpen_your_pencil.py
@@ -4,10 +4,11 @@ evens = []
 for num in data:
     if not num % 2:
         evens.append(num)
+print(evens)
 
 # Done: list comprehension 1
 evens = [ num for num in data if not num % 2 ]
-
+print(evens)
 
 # sharpen your pencil 2
 data = [ 1, 'one', 2, 'two', 3, 'three', 4, 'four' ]
@@ -15,15 +16,19 @@ words = []
 for num in data:
     if isinstance(num, str):
         words.append(num)
+print(words)
 
 # Done: list comprehension 2
 words = [ num for num in data if isinstance(num, str) ]
+print(words)
 
 # sharpen your pencil 3
 data = list('So long and thanks for all the fish'.split())
 title = []
 for word in data:
     title.append(word.title())
+print(title)
 
 # Done: list comprehension 3
 title = [ word.title() for word in data ]
+print(title)
